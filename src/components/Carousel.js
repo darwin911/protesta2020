d2356ec83c2e9e8e760865c7d481e803dcca3e1a
Darwin Smith <darwin911@gmail.com>
2020-02-20 15:08:11 -0500

    moved

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import { images } from './images/index';
+import { images } from '../images/index';
 
 export default () => (
   <Carousel autoPlay centerMode dynamicHeight>
     {images.map(img => (
-      <div>
+      <div key={img}>
         <img src={img} alt='' />
         <div className='legend'>Legend</div>
       </div>
