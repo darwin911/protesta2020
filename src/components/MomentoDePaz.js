db26249834db43d5de6b49c5d5394f3d44d45197
Darwin Smith <darwin911@gmail.com>
2020-02-20 15:08:49 -0500

    creates MomentoDeLaPaz

---
+++ src/components/MomentoDePaz.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const MomentoDePaz = () => {
+  return (
+    <section className='momento-de-paz'>
+      <div className='container'>
+        <h2>Momento de Paz</h2>
+        <h4>
+          A las 6:30pm mientras baje el sol <br />
+          convocamos a los participantes a unirnos en un momento de paz.
+        </h4>
+        <hr />
+
+        <p>
+          Nos sentaremos en el suelo en silencio a recargar energia y a demostrarles a las
+          autoridades, a los que no han podido sumarse a la protesta y a los medios de comunicacion,
+          que esta protesta es pacifica y organizada.
+        </p>
+        <a href='https://www.instagram.com/creoentird/'>@CreoEnTiRD</a>
+      </div>
+    </section>
+  );
+};
