f04e268bdc8351db123ed7626e11a884c96f6b0f
Darwin Smith <darwin911@gmail.com>
2020-02-20 15:09:13 -0500

    imports main components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Carousel from './Carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Subscribe } from './components/Subscribe';
+import { Hero } from './components/Hero';
+import { MomentoDePaz } from './components/MomentoDePaz';
 
 function App() {
   return (
     <div className='App'>
-      <h1 style={{ fontSize: '3em' }}>Protesta 2020</h1>
-      <h3>Plaza de la Bandera</h3>
-      <p>Ya es hora.</p>
-      <div style={{ maxWidth: 800, margin: '0 auto' }}>
-        <Carousel />
+      <div className='App__container'>
+        <Hero />
+        <MomentoDePaz />
+        <Subscribe />
       </div>
     </div>
   );
